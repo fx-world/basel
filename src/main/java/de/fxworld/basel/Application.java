d54b8362af4d8663d23511935f0f6da24ff6a415
Pascal Weyprecht <fx@fx-world.de>
2016-09-17 11:30:22 +0200

    2: Init Spring project
    https://github.com/fx-world/basel/issues/2

---
+++ src/main/java/de/fxworld/basel/Application.java
@@ -0,0 +1,17 @@
+package de.fxworld.basel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }    
+}
