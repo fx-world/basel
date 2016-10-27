8707a8c765dac5dba833f96006ce23479163d1cb
Pascal Weyprecht <fx@fx-world.de>
2016-10-27 21:22:34 +0200

    changed to EclipseLink part 2

--- src/main/java/de/fxworld/basel/Application.java
+++ src/main/java/de/fxworld/basel/Application.java
@@ -1,13 +1,24 @@
 package de.fxworld.basel;
 
+import java.util.Collections;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
 
 @SpringBootApplication
-public class Application {
+public class Application extends JpaBaseConfiguration {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	
@@ -15,5 +26,35 @@ public class Application {
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         log.info("Thank you for using Basel");
+        
     }
+    
+    /**
+	 * @param dataSource
+	 * @param properties
+	 * @param jtaTransactionManagerProvider
+	 */
+	protected Application(DataSource dataSource, JpaProperties properties, ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider) {
+		super(dataSource, properties, jtaTransactionManagerProvider);
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration#createJpaVendorAdapter()
+	 */
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		return new EclipseLinkJpaVendorAdapter();
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration#getVendorProperties()
+	 */
+	@Override
+	protected Map<String, Object> getVendorProperties() {
+
+		// Turn off dynamic weaving to disable LTW lookup in static weaving mode
+		return Collections.singletonMap("eclipselink.weaving", "false");
+}
 }
